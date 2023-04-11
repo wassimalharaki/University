c7c46aa239199352c7b0788050588d704a61c40c
OmarShayya <omar.shayya@std.balamand.edu.lb>
2023-04-11 18:05:40 +0300

    Success ~Omar

--- src/main/java/com/example/university/HelloApplication.java
+++ src/main/java/com/example/university/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        System.out.println("abc");
+        System.out.println("Hello Wassim, I am Omar");
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
