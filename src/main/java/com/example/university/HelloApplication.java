3e83c1ff3633c09ac1008c5b9eb9939bb19bcda8
Wassim Al Haraki <wassimalharaki@gmail.com>
2023-04-12 18:28:27 +0300

    staging test

--- src/main/java/com/example/university/HelloApplication.java
+++ src/main/java/com/example/university/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        System.out.println("Test3");
+        System.out.println("staging");
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
