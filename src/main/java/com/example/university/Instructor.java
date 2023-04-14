e29e2874e571ff4f9eebd0082777867bea2d3ec0
Wassim Al Haraki <wassimalharaki@gmail.com>
2023-04-15 00:02:17 +0300

    Fixed instructor.java

--- src/main/java/com/example/university/Instructor.java
+++ src/main/java/com/example/university/Instructor.java
@@ -1,16 +1,26 @@
 package com.example.university;
 
+import org.hibernate.query.Query;
+
 import java.util.ArrayList;
+import java.util.List;
 
 public class Instructor extends User {
 
-//    public ArrayList<Course> getCoursesInstructed() {
-//        ;
-//    }
-
-    public ArrayList<Student> getStudentsRegisteredInCourse(Course course) {
-        ArrayList<Student> studentsRegisteredInCourse = new ArrayList<>();
-        //TO DO
+    public List<Course> getInstructedCourses() {
+        String hql = "From Course WHERE instructor = :instructor";
+        Query query = Main.session.createQuery(hql);
+        query.setParameter("instructor", this);
+        return (List<Course>) query.getResultList();
+    }
+    public static List<User> getStudentsRegisteredInCourse(Course course) {
+        List<User> studentsRegisteredInCourse = new ArrayList<>();
+        String hql = "From Registration WHERE course = :course";
+        Query query = Main.session.createQuery(hql);
+        query.setParameter("course", course);
+        for (Registration registration: (List<Registration>) query.getResultList()) {
+            studentsRegisteredInCourse.add(registration.getUser());
+        }
         return studentsRegisteredInCourse;
     }
 }
