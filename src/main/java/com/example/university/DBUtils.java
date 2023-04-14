41d12df19b86f386c4d125557d4e542c837f3a1c
OmarShayya <omar.shayya@std.balamand.edu.lb>
2023-04-15 01:18:45 +0300

    finished DBUtils class, added dependency for bycrypt in pom and fixed main class.

--- src/main/java/com/example/university/DBUtils.java
+++ src/main/java/com/example/university/DBUtils.java
@@ -2,42 +2,77 @@ package com.example.university;
 
 import javafx.event.ActionEvent;
 import javafx.event.Event;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Pane;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
 import org.hibernate.query.Query;
+import org.mindrot.jbcrypt.BCrypt;
 
+import java.io.IOException;
 import java.util.List;
 
 public class DBUtils {
 
     public static void changeScene(Event event, String fmxlFile, String title, int width, int height) {
+        Parent root = null;
 
+        try {
+        FXMLLoader loader = new FXMLLoader(DBUtils.class.getResource(fmxlFile));
+        root = loader.load();
+    } catch (IOException e) {
+        e.printStackTrace();
     }
-    public static void login(ActionEvent event, String email, String pass) {
-        String hql = "FROM User WHERE email = :eml and password= :pwd";
 
+    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    Image icon = new Image(DBUtils.class.getResourceAsStream("images/logo.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle(title);
+        stage.setScene(new Scene(root, width, height));
+        stage.setX((Screen.getPrimary().getVisualBounds().getWidth() - stage.getWidth()) / 2);
+        stage.setY((Screen.getPrimary().getVisualBounds().getHeight() - stage.getHeight()) / 2);
+        stage.setResizable(false);
+        stage.show();
+    }
+
+    public static void login(ActionEvent event, String email, String pass) {
+
+        String hql = "FROM User WHERE email = :eml";
         Query query = Main.session.createQuery(hql);
         query.setParameter("eml", email);
-        query.setParameter("pwd", pass);
         List<User> results = query.getResultList();
 
         if (results.size() != 1) {
             Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setContentText("Wrong email or password");
+            alert.setContentText("User not Found");
             alert.show();
             return;
         }
-
-        User current = results.get(0);
-
-        if (current.getRole().equals("s")) {
-            changeScene(event, "LoggedInStudent.fxml", "Welcome Student", 1000, 800);
-        }
-        else if (current.getRole().equals("a")) {
-            changeScene(event, "LoggedInAdmin.fxml", "Welcome Admin", 1000, 800);
-        }
         else {
-            changeScene(event, "LoggedInInstructor.fxml", "Welcome Instructor", 1000, 800);
+            User current = results.get(0);
+            String retrievedPass = current.getPassword();
+            if (BCrypt.checkpw(pass, retrievedPass)) {
+                if (current.getRole().equals("s")) {
+                    changeScene(event, "LoggedInStudent.fxml", "Welcome Student", 1000, 800);
+                } else if (current.getRole().equals("a")) {
+                    changeScene(event, "LoggedInAdmin.fxml", "Welcome Admin", 1000, 800);
+                } else {
+                    changeScene(event, "LoggedInInstructor.fxml", "Welcome Instructor", 1000, 800);
+                }
+            } else{
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setContentText("The provided credentials are incorrect");
+                alert.show();
+                return;
+            }
         }
+
+
     }
 
     public static void signUp(ActionEvent event, String name, String email, String pass, String confPass) {
@@ -63,12 +98,16 @@ public class DBUtils {
         User user = new Student();
         user.setName(name);
         user.setEmail(email);
-        user.setPassword(pass);
+        String hashedPassword = BCrypt.hashpw(pass, BCrypt.gensalt());
+        user.setPassword(hashedPassword);
         user.setRole("s");
+
+
         try {
             Main.session.save(user);
             if (!Main.transaction.getStatus().equals("COMMITTED"))
                 Main.transaction.commit();
+            changeScene(event, "LoggedInStudent.fxml", "Welcome Student", 1000, 800);
         } catch (Exception e) {
             if (Main.transaction != null) {
                 if (!Main.transaction.getStatus().equals("COMMITTED"))
--- src/main/java/com/example/university/Main.java
+++ src/main/java/com/example/university/Main.java
@@ -1,14 +1,17 @@
 package com.example.university;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import org.hibernate.query.Query;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.logging.Level;
 
@@ -25,14 +28,14 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
-        Pane pane = new Pane();
-        Label lbl = new Label("Testing Hibernate");
-        pane.getChildren().add(lbl);
-        System.out.println("success!");
-        Scene scene = new Scene(pane, 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 700, 400);
+        Image icon = new Image(getClass().getResourceAsStream("images/logo.png"));
+        stage.getIcons().add(icon);
+        stage.setTitle("Login");
         stage.setScene(scene);
+        stage.setResizable(false);
         stage.show();
     }
 
