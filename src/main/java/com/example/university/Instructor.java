aaea640fa3fae8f857235066f9a298b38afb4ba1
Wassim Al Haraki <wassimalharaki@gmail.com>
2023-04-14 21:08:39 +0300

    added outline for Instructor.java

--- src/main/java/com/example/university/Instructor.java
+++ src/main/java/com/example/university/Instructor.java
@@ -1,5 +1,22 @@
 package com.example.university;
 
+import java.util.ArrayList;
+
 public class Instructor extends User {
 
+    ArrayList<Course> coursesInstructed = new ArrayList<>();
+
+    public Instructor() {
+        setRole("i");
+    }
+
+    public ArrayList<Course> getCoursesInstructed() {
+        return coursesInstructed;
+    }
+
+    public ArrayList<Student> getStudentsRegisteredInCourse(Course course) {
+        ArrayList<Student> studentsRegisteredInCourse = new ArrayList<>();
+        //TO DO
+        return studentsRegisteredInCourse;
+    }
 }
