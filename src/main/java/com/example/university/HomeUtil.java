7efc8fb6ae0d94412e2d63138fb141d3493d85b7
Wassim Al Haraki <wassimalharaki@gmail.com>
2023-04-15 21:22:02 +0300

    fixed error in signup

--- src/main/java/com/example/university/HomeUtil.java
+++ src/main/java/com/example/university/HomeUtil.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import org.hibernate.query.Query;
+import org.hibernate.resource.transaction.spi.TransactionStatus;
 import org.mindrot.jbcrypt.BCrypt;
 import java.util.List;
 
@@ -78,14 +79,16 @@ public class HomeUtil {
         user.setPassword(hashedPassword);
         user.setRole("s");
 
-        hql = "From User WHERE email = :email";
-        query = Main.session.createQuery(hql);
-        query.setParameter("email", email);
-        int id = ((User) query.getResultList()).getId();
-
         try {
             Main.session.save(user);
-            Main.transaction.commit();
+            if (Main.transaction.getStatus().equals(TransactionStatus.ACTIVE))
+                Main.transaction.commit();
+
+            hql = "From User WHERE email = :email";
+            query = Main.session.createQuery(hql);
+            query.setParameter("email", email);
+            int id = ((User) query.getResultList().get(0)).getId();
+
             Main.id = id;
             changeScene("student.fxml", "Welcome Student", 1000, 800);
         } catch (Exception e) {
